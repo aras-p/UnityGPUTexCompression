617bc47787df6771b8962ea60ce44d465714f0d8
Aras Pranckevicius <aras@nesnausk.org>
2023-07-04 16:08:15 +0300

    Async RMSE readback, report CPU frame times since GPU times are often reported as zeroes (?)

--- Assets/TestGPUTexCompression.cs
+++ Assets/TestGPUTexCompression.cs
@@ -31,7 +31,7 @@ public class TestGPUTexCompression : MonoBehaviour
     private float m_GpuTimesAccum = 0;
     private float m_CpuTimesAccum = 0;
     private int m_FramesAccum = 0;
-    private float m_AvgGpuTime = 0;
+    private float m_AvgFrameTime = 0;
 
     public void Start()
     {
@@ -92,18 +92,10 @@ public class TestGPUTexCompression : MonoBehaviour
         m_DestTexture.Apply(false, true);
     }
 
-    public void Update()
+    void CalculateRMSE()
     {
-        if (m_DestTexture == null || m_RMSEShader == null)
-            return;
-        
-        if (m_Quality != m_CurQuality || m_Format != m_CurFormat)
-        {
-            UpdateDestTexture();
-            UpdateCommandBuffer();
-            m_CurQuality = m_Quality;
-            m_CurFormat = m_Format;
-        }
+        //if (Time.renderedFrameCount % 16 != 0)
+        //    return;
 
         int rmseFactor = 128;
         int bufferSize1 = m_SourceTexture.width * m_SourceTexture.height / rmseFactor;
@@ -124,36 +116,72 @@ public class TestGPUTexCompression : MonoBehaviour
         m_RMSEShader.SetTexture(0, "_TextureB", m_DestTexture);
         m_RMSEShader.SetBuffer(0, "_BufferOutput", m_RMSEBuffer1);
         m_RMSEShader.Dispatch(0, m_SourceTexture.width/128, m_SourceTexture.height, 1);
-        
+
         m_RMSEShader.SetBuffer(1, "_BufferInput", m_RMSEBuffer1);
         m_RMSEShader.SetBuffer(1, "_BufferOutput", m_RMSEBuffer2);
         m_RMSEShader.Dispatch(1, m_RMSEBuffer1.count/128, 1, 1);
 
-        Vector2[] data = new Vector2[m_RMSEBuffer2.count];
-        m_RMSEBuffer2.GetData(data);
-        Vector2 err = Vector2.zero;
-        foreach (var e in data)
-            err += e;
+        AsyncGPUReadback.Request(m_RMSEBuffer2, request =>
+        {
+            if (request.done)
+            {
+                var data = request.GetData<Vector2>();
+                Vector2 err = Vector2.zero;
+                foreach (var e in data)
+                    err += e;
 
-        m_RMSE.x = Mathf.Sqrt(err.x / (m_SourceTexture.width * m_SourceTexture.height)) / 3.0f;
-        m_RMSE.y = Mathf.Sqrt(err.y / (m_SourceTexture.width * m_SourceTexture.height));
+                m_RMSE.x = Mathf.Sqrt(err.x / (m_SourceTexture.width * m_SourceTexture.height)) / 3.0f;
+                m_RMSE.y = Mathf.Sqrt(err.y / (m_SourceTexture.width * m_SourceTexture.height));
+            }
+        });
     }
 
-    void OnGUI()
+    public void Update()
+    {
+        if (m_DestTexture == null || m_RMSEShader == null)
+            return;
+        
+        if (m_Quality != m_CurQuality || m_Format != m_CurFormat)
+        {
+            UpdateDestTexture();
+            UpdateCommandBuffer();
+            m_CurQuality = m_Quality;
+            m_CurFormat = m_Format;
+        }
+
+        CalculateFrameTimes();
+        CalculateRMSE();
+    }
+
+    void CalculateFrameTimes()
     {
         FrameTimingManager.CaptureFrameTimings();
-        FrameTimingManager.GetLatestTimings(1, m_FrameTimings);
-        m_CpuTimesAccum += (float)m_FrameTimings[0].cpuFrameTime;
-        m_GpuTimesAccum += (float)m_FrameTimings[0].gpuFrameTime;
+        if (FrameTimingManager.GetLatestTimings(1, m_FrameTimings) != 1)
+            return; // feature is off, or waiting for GPU readback
+
+        // Looks like it reports GPU times as zeroes when GPU load is "high enough", so that's not
+        // terribly useful (at least on DX11 / GeForce 3080 Ti). Let's report CPU frame time then :(
+        //if (m_FrameTimings[0].gpuFrameTime == 0)
+        //    return; // disjoint frames, GPU restart, high GPU load (?) etc.
+
+        if (m_FrameTimings[0].cpuTimeFrameComplete < m_FrameTimings[0].cpuTimePresentCalled)
+            return; // sanity check
+
+        m_CpuTimesAccum += (float) m_FrameTimings[0].cpuFrameTime;
+        m_GpuTimesAccum += (float) m_FrameTimings[0].gpuFrameTime;
         m_FramesAccum++;
         if (m_GpuTimesAccum > 500 || m_CpuTimesAccum > 500)
         {
-            m_AvgGpuTime = m_GpuTimesAccum / m_FramesAccum;
+            //m_AvgGpuTime = m_GpuTimesAccum / m_FramesAccum;
+            m_AvgFrameTime = m_CpuTimesAccum / m_FramesAccum;
             m_CpuTimesAccum = 0.0f;
             m_GpuTimesAccum = 0.0f;
             m_FramesAccum = 0;
         }
-        
+    }
+
+    void OnGUI()
+    {
         GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(2,2,2));
         
         GUI.Box(new Rect(5, 5, 510, 120), GUIContent.none, GUI.skin.window);
@@ -168,7 +196,7 @@ public class TestGPUTexCompression : MonoBehaviour
         DrawLabelWithOutline(new Rect(170, 40, 100, 20), new GUIContent(m_Quality.ToString("F1")));
         
         // stats
-        var label = $"{m_SourceTexture.width}x{m_SourceTexture.height} {m_Format} q={m_Quality:F1}\nRMSE: RGB {m_RMSE.x:F3}, {m_RMSE.y:F3}\nGPU time: {m_AvgGpuTime:F3}ms ({SystemInfo.graphicsDeviceName} on {SystemInfo.graphicsDeviceType})";
+        var label = $"{m_SourceTexture.width}x{m_SourceTexture.height} {m_Format} q={m_Quality:F1}\nRMSE: RGB {m_RMSE.x:F3}, {m_RMSE.y:F3}\nFrame time: {m_AvgFrameTime:F3}ms ({SystemInfo.graphicsDeviceName} on {SystemInfo.graphicsDeviceType})";
         var rect = new Rect(10, 70, 600, 60);
         DrawLabelWithOutline(rect, new GUIContent(label));
     }
